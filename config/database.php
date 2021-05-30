46a0c36a69fd50a8e997e5b26076d184b6e289b5
edmariooliver <edmariooliver@outlook.com>
2021-05-30 03:13:17 -0300

    Alteraçao no login

--- config/database.php
+++ config/database.php
@@ -45,12 +45,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'url' =>"",
+            'host' => "remotemysql.com",
+            'port' => "3306",
+            'database' => "SXLhEARYws",
+            'username' => "SXLhEARYws",
+            'password' => "gEbGaY0n4O",
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
